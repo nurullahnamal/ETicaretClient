4fba31456758d2ec5b2266e134a9512b52322580
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-05-14 11:12:31 +0300

    alertify

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AlertifyOptions, AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent  implements OnInit{
+  constructor(private alertify: AlertifyService){}
 
+  ngOnInit(): void {
+  }
+
+  m(){
+    this.alertify.message("merhaba",{
+      messageType:MessageType.Success,
+      delay:4,
+      position:Position.BottomRight,
+      dismissOthers:false
+
+    })
+  }
+
+  d(){
+    this.alertify.dismiss();
+  }
 }
