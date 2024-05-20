76a541f01f7ff32b37316972a0fb50789f0e4a50
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-05-20 17:23:49 +0300

    get-post-put-delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminModule } from './admin/admin.module';
 import { UiModule } from './ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { BaseComponent } from './base/base.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,11 +17,14 @@ import { BaseComponent } from './base/base.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    AdminModule,UiModule,
+    AdminModule, UiModule,
     ToastrModule.forRoot(),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: "baseUrl", useValue: "https://localhost:7152/api", multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/ui/components/products/products.component.ts
+++ src/app/ui/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +10,43 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
-  constructor(spinner: NgxSpinnerService) {
+  constructor(spinner: NgxSpinnerService,private httpClientService: HttpClientService ) {
     super(spinner);
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom)
+
+    this.showSpinner(SpinnerType.BallAtom);
+
+    this.httpClientService.get<Product>({
+      controller:"Products"
+    }).subscribe(data=>console.log(data));
+
+
+  /*this.httpClientService.post({
+    controller:"products"
+  },{
+    name:"kalem",
+    stock:100,
+    price:15
+  });
+
+
+  this.httpClientService.put({
+    controller:"products",
+  },{
+    id:"1a56d81b-df03-4356-8da2-25abd906a93a",
+    name:"Renkli kalenm",
+    stock:44
+  }).subscribe()
+
+
+    this.httpClientService.delete({
+      controller: "Products"
+    },"1a56d81b-df03-4356-8da2-25abd906a93a")
+    .subscribe();*/
+
+
+
   }
 }
